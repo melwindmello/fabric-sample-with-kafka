52ecc54e2d55e51cb647a57306f58f94d557c838
Melwin Dmello <melwindmello@capgemini.com>
2018-12-04 16:06:13 +0530

    test

---
+++ first-network/scripts/testQuery.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+CHANNEL_NAME="$1"
+DELAY="$2"
+LANGUAGE="$3"
+TIMEOUT="$4"
+VERBOSE="$5"
+: ${CHANNEL_NAME:="mychannel"}
+: ${DELAY:="3"}
+: ${LANGUAGE:="golang"}
+: ${TIMEOUT:="10"}
+: ${VERBOSE:="false"}
+LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
+COUNTER=1
+MAX_RETRY=5
+
+CC_SRC_PATH="github.com/chaincode/chaincode_example02/go/"
+if [ "$LANGUAGE" = "node" ]; then
+	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/node/"
+fi
+
+echo "Channel name : "$CHANNEL_NAME
+
+# import utils
+. scripts/utils.sh
+
+
+# Invoke chaincode on peer0.org1 and peer0.org2
+#echo "Sending invoke transaction on peer0.org1 peer0.org2..."
+#chaincodeInvoke 0 1 0 2
+
+## Install chaincode on peer1.org2
+#echo "Installing chaincode on peer1.org2..."
+#installChaincode 1 2
+
+# Query on chaincode on peer1.org2, check if the result is 90
+echo "Querying chaincode on peer1.org2..."
+chaincodeQuery 1 2 90
+
+
+exit 0
